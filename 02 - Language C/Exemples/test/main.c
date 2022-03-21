694239600672c7270ee29a686d158905921c982f
AFPA1701871 <martine.poix@afpa.fr>
2022-03-21 17:25:22 +0100

    21/03/2022 17:25:20,77

--- 02 - Language C/Exemples/test/main.c
+++ 02 - Language C/Exemples/test/main.c
@@ -1,43 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    int age ;
-    printf("Entrer votre age");
-    scanf("%d",&age);
-    if (age>=18)
-    {
-        printf("vous etes adulte\n");
-
-    }
-    else
-    {
-        printf("vous etes mineur\n");
-    }
-
-    switch(age)
-    {
-    case 6 :
-    case 7:
-        printf("poussin");
-        break;
-    case 8 :
-        printf("pupille");
-        break;
-    case 9:
-        printf("pupille");
-        break;
-    default:
-        printf("autre");
-        break;
-
-    }
-    printf("\n");
-
-    int i;
-    i = (age>=18)?1:0;
-    printf("%d",i);
 
 
     return 0;
